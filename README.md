dbaa29b374515c5d39a31e024b9da1bf95a54eaa
Juan Galvis <43938697+JuanesGalvis@users.noreply.github.com>
2021-05-07 00:31:29 -0500

    PWA en desarrollo/local funcionando, falta despliegue

---
+++ README.md
@@ -0,0 +1,12 @@
+# 🔢 Bases númericas (PWA) 🔢
+
+> Programación para pasar un número de determinada base a otra
+
+### 🌟 Funcionamiento:
+- Ingresas un número y su respectiva base
+- Ingresas la base a la cual lo quieres convertir
+- Presionas el botón y aparecerá el resultado 👍
+
+### ⌨ Comandos:
+- `npm run build` : Compilar el código
+- `npm run test` : Ejecutar los test programados con Jest
--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [Original, setOriginal] = useState(0);
+  const [BaseOriginal, setBaseOriginal] = useState(0);
+  const [BaseFinal, setBaseFinal] = useState(0);
+  const [Result, setResult] = useState(0);
+
+  function VerificarValores() {
+    if (Original > 0 && BaseOriginal > 0 && BaseFinal > 0) {
+
+      let NumeroDecimal = 0;
+
+      for (let i = 0; i < Original.length; i++) {
+          NumeroDecimal += ( parseInt(Original[(Original.length - 1) - i]) * Math.pow(BaseOriginal, i) );
+      }
+
+      PasarBaseFinal(NumeroDecimal, BaseFinal);
+
+    } else {
+      alert("UNO O VARIOS CAMPOS ESTÁN VACIOS ☣");
+    }
+
+  }
+
+  function PasarBaseFinal( number, basef ) {
+    
+    let BaseFinal = basef;
+    let NumeroFinal = [];
+    let NumeroAConvertir = number;
+
+    for (let i = 0; NumeroAConvertir >= BaseFinal; i++) {
+        
+        NumeroFinal[i] = parseInt(NumeroAConvertir % BaseFinal);
+        NumeroAConvertir = parseInt(NumeroAConvertir / BaseFinal);
+
+    }
+    
+    NumeroFinal.push(NumeroAConvertir)
+    ImprimirResultado(NumeroFinal);
+}
+
+function ImprimirResultado( valorImprimir ) {
+  setResult(valorImprimir.reverse().join(''));
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <React.Fragment>
+      <h1>Convertidor de bases númericas</h1>
+      <header>
+          <label htmlFor="original">
+              Número: 
+              <input id="original" onChange={ (NumeroOriginalInput) => { setOriginal(NumeroOriginalInput.target.value); } } type="number"/>
+          </label>
+          <label htmlFor="baseOriginal">
+              Base Original: 
+              <input id="baseOriginal" onChange={ (BaseOriginalInput) => { setBaseOriginal(BaseOriginalInput.target.value); } } type="number"/>
+          </label>
+          <label htmlFor="baseFinal">
+              Base a pasar: 
+              <input id="baseFinal" onChange={ (BaseFinalInput) => { setBaseFinal(BaseFinalInput.target.value); } } type="number"/>
+          </label>
+          <button id="btn_calcular" onClick={VerificarValores} className="btn">🔢 Calcular 🔢</button>
       </header>
-    </div>
+      <main>
+          <h2 id="result">{ Result ? `Resultado: ${Result}` : '' }</h2>
+      </main>
+    </React.Fragment>
   );
 }
 
